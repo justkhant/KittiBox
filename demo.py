edd1dc4dbd92f1f78da47a09a4db75893f86108b
justkhant <khant.kky@gmail.com>
2019-08-06 15:31:57 -0400

    now takes hdf5 file as input

--- demo.py
+++ demo.py
@@ -14,9 +14,11 @@ Utilizes: Trained KittiBox weights. If no logdir is given,
 pretrained weights will be downloaded and used.
 
 Usage:
-python demo.py --input_image data/demo.png [--output_image output_image]
-                [--logdir /path/to/weights] [--gpus 0]
-
+               
+python demo.py --input_image data/images [or data/images/000000.png] 
+                [--output_dir /path/to/output_images] [--logdir /path/to/weights] 
+                [--gpus GPUs_to_use] [--save_boxes_dir /path/to/bounding_boxes]
+        
 
 """
 from __future__ import absolute_import
@@ -27,6 +29,8 @@ import json
 import logging
 import os
 import sys
+import h5py
+from skimage import exposure
 
 import collections
 
@@ -94,6 +98,28 @@ def maybe_download_and_extract(runs_dir):
 
     return
 
+def image_transform(image, hypes):
+
+    #make 3 channels
+    image = np.expand_dims(image, axis=2)
+    image = np.tile(image, (1, 1, 3))
+    
+    #slice off the hood
+    image = image[0:198,:,:]
+    
+    #resize image
+    original_height, _, _ = image.shape
+    new_width = int(hypes["image_width"] * original_height / hypes["image_height"])
+    image = scp.misc.imresize(image, (hypes["image_height"], new_width),
+                              interp='cubic')
+
+    #pad sides
+    pad_width_1 = int((hypes["image_width"] - new_width) / 2)
+    pad_width_2 = hypes["image_width"] - new_width - pad_width_1
+    image = np.pad(image, [(0, 0), (pad_width_1, pad_width_2), (0, 0)], mode='constant')
+
+    image = exposure.adjust_gamma(image, 0.5) 
+    return image 
 
 def main(_):
     tv_utils.set_gpus_to_use()
@@ -154,80 +180,96 @@ def main(_):
         input_images = os.listdir(image_input)
         input_images = [os.path.join(image_input, x) for x in input_images]
     else:
-        input_images = [image_input] 
+        if (image_input.endswith(".hdf5")):
+            hdf5_file = h5py.File(image_input, "r")
+            input_images = hdf5_file["davis"]["left"]["image_raw"] 
+            input_images = [input_images[i] for i in range(len(input_images)) if i % 60 == 0]  
 
-    for input_image in input_images:
-        if input_image.endswith(".png"):
-            logging.info("Starting inference using {} as input".format(input_image))
+        else:
+            input_images = [image_input] 
+
+    for i, input_image in enumerate(input_images):
+
+        if not isinstance(input_image, basestring):
+            image = image_transform(input_image, hypes)
+            input_image = "image{:04}".format(i)
+
+        elif (input_image.endswith(".png")):
 
             # Load and resize input image
             image = scp.misc.imread(input_image)
             image = scp.misc.imresize(image, (hypes["image_height"],
                                               hypes["image_width"]),
                                       interp='cubic')
-            feed = {image_pl: image}
-
-            # Run KittiBox model on image
-            pred_boxes = prediction['pred_boxes_new']
-            pred_confidences = prediction['pred_confidences']
-            (np_pred_boxes, np_pred_confidences) = sess.run([pred_boxes,
-                                                             pred_confidences],
-                                                            feed_dict=feed)
-            
-            # Apply non-maximal suppression
-            # and draw predictions on the image
-            output_image, rectangles = kittibox_utils.add_rectangles(
-                hypes, [image], np_pred_confidences,
-                np_pred_boxes, show_removed=False,
-                use_stitching=True, rnn_len=1,
-                min_conf=0.50, tau=hypes['tau'], color_acc=(0, 255, 0))
-
-            threshold = 0.5
-            accepted_predictions = []
-            # removing predictions <= threshold
-            for rect in rectangles:
-                if rect.score >= threshold:
-                    accepted_predictions.append(rect)
-
-            print('')
-            logging.info("{} Cars detected".format(len(accepted_predictions)))
-
-            boxes_coords = []
-            
-            # Printing coordinates of predicted rects.
-            for i, rect in enumerate(accepted_predictions):
-                logging.info("")
-                logging.info("Coordinates of Box {}".format(i))
-                logging.info("    x1: {}".format(rect.x1))
-                logging.info("    x2: {}".format(rect.x2))
-                logging.info("    y1: {}".format(rect.y1))
-                logging.info("    y2: {}".format(rect.y2))
-                logging.info("    Confidence: {}".format(rect.score))
-
-                boxes_coords.append("Car 0.00 0 0.00 {} {} {} {} 0 0 0 0 0 0 0 {}\n".format(rect.x1, rect.y1, rect.x2, rect.y2, rect.score))  
-
-            #save boxes    
-            if FLAGS.save_boxes_dir is not None:
-                boxes_filename = os.path.join(FLAGS.save_boxes_dir, input_image.split('/')[-1].split('.')[0] + '_boxes.txt') 
-                boxes_file = open(boxes_filename, "w")
-                boxes_file.writelines(boxes_coords)
-                boxes_file.close()
-                logging.info("") 
-                logging.info("Boxes saved to {}".format(boxes_filename))
-                
-            # save Image
-            if FLAGS.output_dir is not None:
-                output_name = os.path.join(FLAGS.output_dir, input_image.split('/')[-1].split('.')[0] + '_rects.png') 
-                scp.misc.imsave(output_name, output_image)
-                logging.info("")
-                logging.info("Output image saved to {}".format(output_name))
-
-            logging.info("")
-
+    
         else:
             logging.info("{} is not in .png format. Edit in demo.py to accept other input file types".format(input_image))
             logging.info("")
+            exit(1)
+            
+        logging.info("Starting inference using {} as input".format(input_image))
+        
+        feed = {image_pl: image}
+        
+        # Run KittiBox model on image
+        pred_boxes = prediction['pred_boxes_new']
+        pred_confidences = prediction['pred_confidences']
+        (np_pred_boxes, np_pred_confidences) = sess.run([pred_boxes,
+                                                        pred_confidences],
+                                                        feed_dict=feed)
+        
+        # Apply non-maximal suppression
+        # and draw predictions on the image
+        output_image, rectangles = kittibox_utils.add_rectangles(
+            hypes, [image], np_pred_confidences,
+            np_pred_boxes, show_removed=False,
+            use_stitching=True, rnn_len=1,
+            min_conf=0.5, tau=hypes['tau'], color_acc=(0, 255, 0))
+        
+        threshold = 0
+        accepted_predictions = []
+        
+        # removing predictions <= threshold
+        for rect in rectangles:
+            if rect.score >= threshold:
+                accepted_predictions.append(rect) 
+            
+        print('')
+        logging.info("{} Cars detected".format(len(accepted_predictions)))
 
+        boxes_coords = []
+            
+        # Printing coordinates of predicted rects.
+        for i, rect in enumerate(accepted_predictions):
+            logging.info("")
+            logging.info("Coordinates of Box {}".format(i))
+            logging.info("    x1: {}".format(rect.x1))
+            logging.info("    x2: {}".format(rect.x2))
+            logging.info("    y1: {}".format(rect.y1))
+            logging.info("    y2: {}".format(rect.y2))
+            logging.info("    Confidence: {}".format(rect.score))
+                
+            boxes_coords.append("Car 0.00 0 0.00 {} {} {} {} 0 0 0 0 0 0 0 {}\n".format(rect.x1, rect.y1, rect.x2, rect.y2, rect.score))  
+                
+        #save boxes    
+        if FLAGS.save_boxes_dir is not None:
+            boxes_filename = os.path.join(FLAGS.save_boxes_dir, input_image.split('/')[-1].split('.')[0] + '_boxes.txt') 
+            boxes_file = open(boxes_filename, "w")
+            boxes_file.writelines(boxes_coords)
+            boxes_file.close()
+            logging.info("") 
+            logging.info("Boxes saved to {}".format(boxes_filename))
+                
+        # save Image
+        if FLAGS.output_dir is not None:
+            output_name = os.path.join(FLAGS.output_dir, input_image.split('/')[-1].split('.')[0] + '_rects.png') 
+            scp.misc.imsave(output_name, output_image)
+            logging.info("")
+            logging.info("Output image saved to {}".format(output_name))
+
+            logging.info("")
+
+        
  #   logging.warning("Do NOT use this Code to evaluate multiple images.")
 
  #   logging.warning("Demo.py is **very slow** and designed "
